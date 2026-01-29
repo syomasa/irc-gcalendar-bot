818960075cb888b72fb3a613ec21281d6774cb91
Santeri Hyvärinen <santeri.hyvarinen12@gmail.com>
2026-01-29 15:23:51 +0200

    Add runtime guards to on_response and improve documentation

--- src/runner.py
+++ src/runner.py
@@ -8,11 +8,30 @@ T = TypeVar("T", covariant=True)
 P = ParamSpec("P")
 
 
-def on_response(response_code: str):
+def _on_response(target_response_code: str):
+    """
+    Decorator factory responsible of constructing decorators
+    which can be hooked into a arbittuary event (for irc specific numerics/events check RFC2812)
+    """
+
     def decor_wrapper(func: Callable[P, T]) -> Callable[P, T | None]:
         @wraps(func)
         def func_wrapper(*args: P.args, **kwargs: P.kwargs):
-            if len(args) > 1 and args[1] == response_code:
+            # Ensure that there is two arguments instance (self) and msg (str)
+            # this decorator expects these two to exist to work properly.
+            if len(args) < 2:
+                return None
+
+            # This decorator assumes that msg (args[1]) is decoded before (i.e type(args[1]) == str)
+            # handed over to parser, don't change unless absolutely necessary
+            if not isinstance(args[1], str):
+                raise TypeError(
+                    f"Args[1] is expected to be string but got {type(args[1])} instead"
+                )
+
+            msg: str = args[1]
+            response_code: str = _MessageParser.get_response_code(args[1])
+            if response_code == target_response_code:
                 return func(*args, **kwargs)
 
             return None
@@ -20,6 +39,15 @@ def on_response(response_code: str):
         return func_wrapper
 
     return decor_wrapper
+
+
+class _MessageParser:
+    @staticmethod
+    def get_response_code(msg: str):
+        raise NotImplemented
+
+    def parse_lines(self, msg: bytes):
+        raise NotImplemented
 
 
 class BotRunner:
@@ -37,25 +65,23 @@ class BotRunner:
 
     def __init__(self, client: IRCClient):
         self.client = client
-        self.state = ...
 
     def _initialize_connection(self):
         self.client.connect()
         self.client.send_credentials()
 
-    def _parse_msg(self):
-        pass
-
-    @on_response("PING")
+    @_on_response("PING")
     def _handle_ping(self, msg: str) -> None:
         # TODO: Parse answer from msg
 
         answer: str = ""
         self.client.pong(answer)
 
+    @_on_response("352")  # numeric of WHO is 352
     def _handle_who(self, msg: str) -> None:
         raise NotImplemented
 
+    @_on_response("001")  # numeric of Welcome is 001
     def _handle_welcome(self, msg: str) -> None:
         raise NotImplemented
 
@@ -69,7 +95,3 @@ class BotRunner:
         self._handle_ping(tmp)
         self._handle_welcome(tmp)
         self._handle_who(tmp)
-
-    @on_response("001")
-    def test_msg(self, msg: str) -> None:
-        print(msg)
